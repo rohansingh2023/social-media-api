da96a273b3ec8c2e1410ed399bf8b04477a23630
Rohan Singh <65129632+rohansingh2023@users.noreply.github.com>
2023-03-29 17:47:32 +0530

    commited changes

--- dist/graphql/resolvers/index.js
+++
@@ -1,6 +0,0 @@
-import Query from "./queries.js";
-import Mutation from "./mutations.js";
-export default {
-  Query,
-  Mutation,
-};
--- dist/models/index.js
+++
@@ -1,6 +0,0 @@
-import Post from "./Post.js";
-import User from "./User.js";
-import Conversation from "./Conversation.js";
-import Message from "./Message.js";
-const models = { Post, User, Conversation, Message };
-export default models;
