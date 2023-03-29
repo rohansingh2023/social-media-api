60729c902265495e2948d82c1c4d67052385bc3f
Rohan Singh <65129632+rohansingh2023@users.noreply.github.com>
2023-03-29 18:43:18 +0530

    commited changes 5

--- dist/db.js
+++ dist/db.js
@@ -4,7 +4,9 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const mongoose_1 = __importDefault(require("mongoose"));
-const DBM_URL = "mongodb://localhost:27017/graphqlSmDB?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false";
+const dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config();
+const DBM_URL = "mongodb+srv://rohan:1kHZzHhbqTdEuCQn@cluster0.sgvxcek.mongodb.net/?retryWrites=true&w=majority";
 const dbConnect = async () => {
     try {
         await mongoose_1.default.connect(DBM_URL, {});
